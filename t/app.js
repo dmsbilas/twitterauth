4cd203fa84e45f9e1a3976e8c76e77a260810601
Bilas Siddiq <dmsbilas@yahoo.com>
2017-08-21 17:58:33 +0600

    added html view engine instead of jade

--- t/app.js
+++ t/app.js
@@ -5,14 +5,18 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var cons = require('consolidate');
+
 var index = require('./routes/index');
 var users = require('./routes/users');
+
 
 var app = express();
 
 // view engine setup
+app.engine('html', cons.swig)
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
